5cad6dcbe3e182f8a6ce39033d976e2f4cf786a8
mohammadazadbar1995 <mohammadazadbar1373@gmail.com>
2023-12-04 13:10:08 +0330

    [feature] add view model to project

--- app/src/main/java/ir/state/compose/ui/component/WellnessScreen.kt
+++ app/src/main/java/ir/state/compose/ui/component/WellnessScreen.kt
@@ -2,34 +2,31 @@ package ir.state.compose.ui.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ir.state.compose.model.WellnessTask
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ir.state.compose.viewmodel.WellnessViewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
 
     Column(modifier = modifier) {
         StatefulCounter(modifier = modifier)
 
-        val list = remember {
-            getWellnessTasks().toMutableStateList()
-        }
         WellnessTasksList(
-            onCloseTask = { task -> list.remove(task) },
-            list = list,
+            onCloseTask = { task -> wellnessViewModel.remove(task) },
+            list = wellnessViewModel.tasks,
             modifier = modifier,
         )
     }
 }
 
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
-
 
 @Preview
 @Composable
 fun PreviewWellnessScreen() {
-    WellnessScreen()
+//    WellnessScreen()
 }
