bc522d6c9ecf68d8d913b8a35eba6d6b9abb5926
mohammadazadbar1995 <mohammadazadbar1373@gmail.com>
2023-12-04 12:36:46 +0330

    [feature] add close button and remove item

--- app/src/main/java/ir/state/compose/ui/component/WellnessScreen.kt
+++ app/src/main/java/ir/state/compose/ui/component/WellnessScreen.kt
@@ -1,13 +1,32 @@
 package ir.state.compose.ui.component
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ir.state.compose.model.WellnessTask
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier = modifier)
+
+    Column(modifier = modifier) {
+        StatefulCounter(modifier = modifier)
+
+        val list = remember {
+            getWellnessTasks().toMutableStateList()
+        }
+        WellnessTasksList(
+            onCloseTask = { task -> list.remove(task) },
+            list = list,
+            modifier = modifier,
+        )
+    }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
 
 @Preview
 @Composable
